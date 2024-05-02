ad0ed3cc3666adf5e6f0427246495b0812d2389d
Yogesh Singla <monusingla104@gmail.com>
2024-05-02 18:27:31 +0530

    socket url update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
 
     useEffect(() => {
-        const socket = io("http://localhost:8000", {
+        const socket = io("https://chat-book-cyzn.onrender.com", {
             query: {
                 userId: user?._id,
             },
