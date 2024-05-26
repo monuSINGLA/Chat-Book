207d0700851799ba5e16178a61493c4de830e0d8
Yogesh Singla <monusingla104@gmail.com>
2024-05-26 22:21:53 +0530

    testing middle ware

--- backend/middleware/blockUserAgent.js
+++ backend/middleware/blockUserAgent.js
@@ -3,7 +3,7 @@ function blockPentestTools(req, res, next) {
     const referer = req.headers['referer'];
 
     // Check if the request's referer header contains the pentest-tools.com domain
-    if (referer && referer.toLowerCase().includes('pentest-tools.com')) {
+    if (referer && referer.toLowerCase().includes('app.pentest-tools.com')) {
         // Respond with a 403 Forbidden status code
         return res.status(403).send('Forbidden');
     }
